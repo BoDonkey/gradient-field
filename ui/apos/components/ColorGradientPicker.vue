b064f519caaea9b53f87911ee2069625924875ef
Bob Means (BoDonkey) <robert@apostrophecms.com>
2022-09-28 09:14:50 -0400

    new tack

--- ui/apos/components/ColorGradientPicker.vue
+++ ui/apos/components/ColorGradientPicker.vue
@@ -1,32 +1,27 @@
 <template>
-  <AposInputWrapper
-    :field="field"
-    :error="null"
-    :uid="uid"
-    :display-options="displayOptions"
-    :modifiers="modifiers"
-    :value="value"
-  >
+  <AposInputWrapper :field="field" :error="null" :uid="uid" :display-options="displayOptions" :modifiers="modifiers"
+    :value="value">
     <template #body>
       <div class="apos-input-object">
         <div class="apos-input-wrapper">
           <div id="color-square" :style="{ background: gradient }" />
           <div>
-            <AposInputRange
-            :field="{
+            <AposInputRange :field="{
               name: 'angle',
               label: 'Gradient Angle',
-            }"
-            :value="value"
-            @change="gradientChange"
-            />
-            </div>
+              min: 0,
+              max: 360,
+              unit: 'deg',
+              def: 90
+            }" v-model="angleValue" :conditionMet="true" />
+          </div>
           <div>
-            <!-- <AposSchema
-            :schema="extraColorSchema" v-model="colorsSchemaInput"
-            :trigger-validation="triggerValidation"
-            :utility-rail="false" :generation="generation"
-            /> -->
+            <ul>
+              <li v-for="(color, index) in colors">
+                <AposSchema :schema="extraColorSchema" v-model="colorValue" :trigger-validation="triggerValidation"
+                  :utility-rail="false" :generation="generation" :key="index" />
+              </li>
+            </ul>
           </div>
           <footer class="apos-link-control__footer">
             <AposButton type="button" label="+" :disabled="addLimit" @click="addColor" />
@@ -38,6 +33,7 @@
 </template>
 
 <script>
+
 import AposInputMixin from 'Modules/@apostrophecms/schema/mixins/AposInputMixin';
 import AposInputWrapper from 'Modules/@apostrophecms/schema/components/AposInputWrapper.vue';
 
@@ -45,8 +41,8 @@ export default {
   name: 'ColorGradientPicker',
   components: {
     AposInputWrapper
-},
-  mixins: [ AposInputMixin ],
+  },
+  mixins: [AposInputMixin],
   props: {
     generation: {
       type: Number,
@@ -58,11 +54,21 @@ export default {
   },
   data() {
     const next = this.getNext();
+    const colorSchema = this.getColorSchema();
+    const colorsLength = next.colors.length;
+    console.log('in data', colorsLength);
+    console.log('next in data', next);
     return {
-      colorsSchemaInput: {
+      extraColorSchema: colorSchema,
+      colorValue: {
         data: next
       },
-      next
+      angleValue: {
+        data: next.angle
+      },
+      next,
+      colors: next.colors,
+      colorsLength: colorsLength
     };
   },
   computed: {
@@ -77,6 +83,27 @@ export default {
     }
   },
   watch: {
+    angleValue() {
+      console.log('in angle value', this.angleValue, this.next);
+      this.next = {
+        ...this.next,
+        ...this.angleValue,
+        angle: this.angleValue.data
+      }
+    },
+    colorValue() {
+      console.log('in color picker', this.colorValue, this.next);
+      const newColorArray = this.setColorArray();
+      const colorsLength = newColorArray.length;
+      console.log('in colorValue', typeof newColorArray, newColorArray,);
+      this.next = {
+        ...this.next,
+        ...this.colorValue,
+        color: this.colorValue.data.color,
+        colors: newColorArray,
+        colorsLength: colorsLength
+      }
+    },
     generation() {
       this.next = this.getNext();
       this.colorsSchemaInput = {
@@ -86,13 +113,37 @@ export default {
   },
   methods: {
     validate(value) {
-      if (this.colorsSchemaInput.hasErrors)
-      {
-        return 'invalid';
-      }
+      return false;
     },
     getNext() {
-      return this?.value.data ? this.value.data : (this.field.def || {});
+      console.log('in getNext', this.value.data);
+      return this.value.data ? this.value.data : (this.field.def || {
+        angle: 90,
+        color: '#4a90e2ff',
+        colors: ['#4a90e2ff'],
+        ranValidation: true
+      });
+    },
+    getAngleValue() {
+      console.log('in getAngleValue');
+      return this?.value.angle ? this.value.angle : (this.field.def || 90);
+    },
+    setColorArray() {
+      console.log('in setColorArray length',this.colorsLength);
+      // Return new color if we are changing the first color
+      if (this.colorsLength === 1){
+        return  [ this.colorValue.data.color ];
+      };
+
+      let colorArray = this.next.colors;
+      console.log('in set color array', typeof colorArray, colorArray);
+
+      // Return changed array if we are changing in place
+      if (colorArray.length === this.colorsLength) {
+        return colorArray[colorArray.length - 1] = this.colorValue.data.color;
+      } else {
+        return colorArray.push(this.colorValue.data.color);
+      }
     },
     parseSchema() {
       const defaultSchema = [
@@ -113,7 +164,7 @@ export default {
       ];
       return defaultSchema;
     },
-    colorSchema() {
+    getColorSchema() {
       const returnedSchema = [
         {
           name: 'color',
@@ -124,11 +175,15 @@ export default {
     },
     addColor() {
       // How do I add an extra AposSchema?
-      console.log('tbd');
+      console.log('prepush', this.next, this.colorsLength);
+      let newColorArray = [...this.next.colors];
+      newColorArray.push("#00ff00ff");
+
+      console.log('in plus', this.next.colors);
     },
-    gradientChange() {
+    angleChange() {
       this.error = this.value.error;
-      console.log('gradient error', this.error);
+      console.log('angle error', this.error);
       this.next.angle = this.data;
     }
   }
